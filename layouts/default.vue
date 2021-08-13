84c29f9d706492d65f33cfc3e889c7e7c8729fa7
bimer95 <shevchenkoVEN@yandex.ru>
2021-08-13 11:49:12 +0700

    smart

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="primary">
+    <div class="primary__template template">
+      <div class="template__header">
+      </div>
+      <div class="template__content">
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+  },
+};
+</script>
+<style lang="scss" scoped>
+.primary {
+  height: 100vh;
+  overflow-y: auto;
+  &__template {
+    height: 100%;
+    display: grid;
+    grid-template-rows: 100px 1fr auto;
+  }
+}
+
+.template {
+  height: 100%;
+  min-height: 100vh;
+  overflow: auto;
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+  }
+}
+</style>
